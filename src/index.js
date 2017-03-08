99dcd879e6b7b4bea4e63a04d29f25a470523e65
Jingbo Zhang <jb.zhang1234@gmail.com>
2017-03-07 21:43:31 -0500

    es6

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// function Person(name, age) {
+//   if(typeof name === 'undefined') name=name || "Charleszhang"
+//   if(typeof age === 'undefined') age = age || "18"
+//
+//   return name + age;
+// }
+//
+//
+// //ES6
+// var Person = (name="Charleszhang",age="18") => name + ' ' + age
+//
+// Person("Chareszhang");
+
+//Keys 任意参数
+
+import module from "./module"
+console.log(module);
---
+++ src/module.js
@@ -0,0 +1,9 @@
+
+export var a = 1;
+export let obj = {
+  m: "2"
+}
+
+export function fn(a,b){
+  console.log(a + b)
+}
